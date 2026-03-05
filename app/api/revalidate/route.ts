6513b8f679dfb44e320b84905b0a8a5cff7a765a
shivanijaswal873 <shivanijaswal482@gmail.com>
2026-03-05 18:22:12 +0530

    added webhook

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,8 +1,16 @@
-import { NextResponse } from "next/server";
-import { revalidateTag } from "next/cache";
+import { NextRequest, NextResponse } from "next/server";
+import { revalidatePath } from "next/cache";
 
-export async function POST() {
-  revalidateTag("prismic", "max");
+export async function POST(req: NextRequest) {
+  try {
+    revalidatePath("/");
+    revalidatePath("/contact");
 
-  return NextResponse.json({ revalidated: true, now: Date.now() });
+    return NextResponse.json({ revalidated: true });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Error revalidating" },
+      { status: 500 }
+    );
+  }
 }
