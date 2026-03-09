4d181a5e88e508cb332817aeff4ea48febb68729
shivanijaswal873 <shivanijaswal482@gmail.com>
2026-03-09 14:09:04 +0530

    Added SCSS style for the  blog scetion

--- app/blog/page.js
+++ app/blog/page.js
@@ -2,8 +2,6 @@ import { createClient } from "@/prismicio";
 import BlogCard from "../components/BlogCard";
 import styles from "./page.module.scss";
 import { PrismicRichText } from "@prismicio/react";
-import { components } from "@/slices";
-import { SliceZone } from "@prismicio/react";
 
 export default async function BlogPage() {
   const client = createClient();
@@ -13,22 +11,27 @@ export default async function BlogPage() {
 
   return (
     <section className={styles.blogPage}>
-      <div className={styles.container}>
+      <div className={styles.blogPage__container}>
+        
         {blogPage?.data?.subheading && (
-          <p className={styles.subHeading}>{blogPage.data.subheading}</p>
+          <p className={styles.blogPage__subHeading}>
+            {blogPage.data.subheading}
+          </p>
         )}
 
         {blogPage?.data?.heading && (
-          <h1 className={styles.heading}>{blogPage.data.heading}</h1>
+          <h1 className={styles.blogPage__heading}>
+            {blogPage.data.heading}
+          </h1>
         )}
 
         {blogPage?.data?.description?.length > 0 && (
-          <div className={styles.description}>
+          <div className={styles.blogPage__description}>
             <PrismicRichText field={blogPage.data.description} />
           </div>
         )}
 
-        <div className={styles.grid}>
+        <div className={styles.blogPage__grid}>
           {blogs.map((blog) => (
             <BlogCard key={blog.id} blog={blog} />
           ))}
